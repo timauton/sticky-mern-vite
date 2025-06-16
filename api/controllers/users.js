341fb54c70d936c9331188e552e3f40dcf7de038
bridgetkalinda24 <bridgetkalinda@yahoo.co.uk>
2025-06-16 17:04:46 +0100

    Initial commit for the user data model files

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,24 +1,49 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-function create(req, res) {
-  const email = req.body.email;
-  const password = req.body.password;
+const JWT_CODE = process.env.JWT_CODE || "supersecret";
 
-  const user = new User({ email, password });
-  user
-    .save()
-    .then((user) => {
-      console.log("User created, id:", user._id.toString());
-      res.status(201).json({ message: "OK" });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(400).json({ message: "Something went wrong" });
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ message: "User has an account" });
+
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
     });
-}
 
-const UsersController = {
-  create: create,
+    res.status(201).json({ message: "User registered", user: { id: user._id, username: user.username } });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
 };
 
-module.exports = UsersController;
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Inavalid email or password" });
+
+    const isAMatch = await bcrypt.compare(password, user.password);
+    if (!isAMatch) return res.status(400).json({ message: "Inavalid email or password" });
+
+    const token = jwt.sign({ userId: user._id }, JWT_CODE, { expiresIn: "1d" });
+
+    res.status(200).json({ token, user: { id: user._id, username: user.username } });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
+module.exports = {
+  registerUser,
+  login,
+}
