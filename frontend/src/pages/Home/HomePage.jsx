f6ccd0b7de70ce314a267a2e1a86be347450e0e6
Sarah Mathieson <sarahmathieson@nationwide.co.uk>
2025-06-16 17:52:21 +0100

    links and image frame in place

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 
-import "./HomePage.css";
+import "../../index.css";
 
 export function HomePage() {
   return (
-    <div className="home">
-      <h1>Welcome to Acebook!</h1>
-      <Link to="/signup">Sign Up</Link>
-      <Link to="/login">Log In</Link>
+    <div className="view">
+      <div className="signup-and-login">
+      <Link to="/signup" className="signup-link">Sign Up</Link>
+      <Link to="/login" className="login-link">Log In</Link>
+      </div>
+      <h2>It's Sticky!</h2>
+      <div className="image-frame">
+        <img src="/The-archives.jpg" alt="the archives" />
+      </div>
     </div>
   );
 }
