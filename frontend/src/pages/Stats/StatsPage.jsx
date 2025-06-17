ac1fd5e0205a15f3b7a3c7d78d0de71433297505
Steve Cottle <stevocottle@gmail.com>
2025-06-17 13:01:23 +0100

    WIP stats page styling - added avatar and charts

--- frontend/src/pages/Stats/StatsPage.jsx
+++ frontend/src/pages/Stats/StatsPage.jsx
@@ -14,30 +14,26 @@ export const StatsPage = () => {
             <h3>This is one of my memes</h3>
           </div>
           <div className="my-meme">
-            <img src="/temp-images/temp-meme-image.jpeg" alt="Meme 1" />
+            <img src="/temp-images/temp-meme-image.jpeg" alt="Meme 2" />
             <h3>This is one of my memes</h3>
           </div>
           <div className="my-meme">
-            <img src="/temp-images/temp-meme-image.jpeg" alt="Meme 1" />
-            <h3>This is one of my memes</h3>
-          </div>
-          <div className="my-meme">
-            <img src="/temp-images/temp-meme-image.jpeg" alt="Meme 1" />
+            <img src="/temp-images/temp-meme-image.jpeg" alt="Meme 3" />
             <h3>This is one of my memes</h3>
           </div>
         </div>
       </div>
       <div className="card my-profile-card">
         <h2>My Profile</h2>
-        <h3>A piece of profile information</h3>
-        <h3>A piece of profile information</h3>
-        <h3>A piece of profile information</h3>
+        <img className="avatar" src="/temp-images/avatar.jpg" alt="User avatar" />
+        <h3>username</h3>
+        <h3>password</h3>
       </div>
       <div className="card stats-card">
         <h2>My Charts</h2>
-        <h3>Stats chart 1</h3>
-        <h3>Stats chart 2</h3>
-        <h3>Stats chart 3</h3>
+        <img src="/temp-images/chart1.png" alt="Chart 1"/>
+        <img src="/temp-images/chart2.png" alt="Chart 2"/>
+        <img src="/temp-images/chart3.png" alt="Chart 3"/>
       </div>
     </div>
   );
