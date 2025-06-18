53e87ed538e2e6708b6d049cb315ba8080993116
Peter Nieuwkoop <peternieuwkoop94@gmail.com>
2025-06-18 10:48:33 +0100

    minor login changes

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,16 +1,21 @@
 import { Link } from "react-router-dom";
+import Button from "../../components/ButtonComponent"
+import Login from "../../components/Login"
 import LogoutButton from "../../components/LogoutButton"
 
 import "../../index.css";
 
 // logged out render
 export function HomePage() {
+
+
   return (
     <div className="view">
       <div className="signup-and-login">
       <Link to="/signup" className="signup-link">Sign Up</Link>
-      <Link to="/login" className="login-link">Log In</Link>
-      <LogoutButton />
+      {/* <Link to="/login" className="login-link">Log In</Link> */}
+      <div className="form-container"></div>
+      <Button className="login-button" buttonText={"Login"}/>
       </div>
       <div className="title">It is Sticky!</div>
       <div className="image-container">
