d56e2f26ccef9820802d556b0d91c6cc70dd5347
Steve Cottle <stevocottle@gmail.com>
2025-06-18 16:50:37 +0100

    WIP: Initial commit for 'image upload'

---
+++ frontend/src/components/MemeUploadComponent.jsx
@@ -0,0 +1,23 @@
+
+
+
+
+const MemeUpload = () => {
+    return(
+        <div className="meme-upload-wrapper">
+            <div className="meme-upload-title">
+                <h3>Upload your meme</h3>
+            </div>
+            
+            <form className="meme-upload-form">
+                    <input type="text" placeholder="Meme title"/>
+                    <input type="file" accept="image/*"/>
+                    <input type="submit" />
+            </form>
+
+        </div>
+
+    )
+};
+
+export default MemeUpload;
