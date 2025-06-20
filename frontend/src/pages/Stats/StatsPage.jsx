a9d504b3165e812907105114162ad4f20a29bd1d
trudie anderson <trudiekennedy@gmail.com>
2025-06-20 18:11:14 +0100

    Fixed failing test for statsPage (missing heading level). Removed unused import from statsPage

--- frontend/src/pages/Stats/StatsPage.jsx
+++ frontend/src/pages/Stats/StatsPage.jsx
@@ -1,5 +1,4 @@
 import Button from "../../components/ButtonComponent";
-import MemeUpload from "../../components/MemeUploadComponent";
 import { useNavigate } from "react-router-dom";
 
 export const StatsPage = () => {
