4c2e27d32a16e0bb235619cd1b5f586301ddfdd3
Sarah Mathieson <sarahmathieson@nationwide.co.uk>
2025-06-23 16:06:57 +0100

    basic styling completed

--- frontend/src/pages/Stats/StatsPage.jsx
+++ frontend/src/pages/Stats/StatsPage.jsx
@@ -5,59 +5,62 @@ export const StatsPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="stats-page-wrapper">
+   <>
+    <div className="background-image"></div>
+    <div className="background-area">
+      <div className="stats-page-wrapper">
 
-      <Button
-        className="homepage-nav-button"
-        buttonText="Back to Memes" // other text is available
-        onClick={() => navigate("/")}
-      />
+        <Button
+          className="homepage-nav-button"
+          buttonText="Back to Memes" // other text is available
+          onClick={() => navigate("/")}
+        />
 
-      <div className="stats-title">
-        <h1>Stats page</h1>
-      </div>
+        <div className="stats-title">Stats page</div>
 
-      <div className="row">
+        <div className="row">
 
-      <div className="column">
-        <div className="card my-profile-card">
-          <h2>My Profile</h2>
-          <img className="avatar" src="/temp-images/avatar.jpg" alt="User avatar" />
-          <h3>username</h3>
-          <h3>password</h3>
+        <div className="column-left">
+          <div className="card my-profile-card">
+            <h2>My Profile</h2>
+            <img className="avatar" src="/temp-images/avatar.jpg" alt="User avatar" />
+            <h3>username</h3>
+            <h3>password</h3>
+          </div>
+
+          <div className="card stats-card">
+            <h2>My Charts</h2>
+            <img src="/temp-images/chart1.png" alt="Chart 1"/>
+            <img src="/temp-images/chart2.png" alt="Chart 2"/>
+            <img src="/temp-images/chart3.png" alt="Chart 3"/>
+          </div>
         </div>
 
-        <div className="card stats-card">
-          <h2>My Charts</h2>
-          <img src="/temp-images/chart1.png" alt="Chart 1"/>
-          <img src="/temp-images/chart2.png" alt="Chart 2"/>
-          <img src="/temp-images/chart3.png" alt="Chart 3"/>
+        <div className="column-right">
+          <div className="card my-memes-card">
+            <div className="my-memes">
+              <h2>My Memes</h2>
+              <div className="my-meme">
+                <img src="/temp-images/temp-meme-image.jpeg" alt="Meme 1" />
+                <h3>This is one of my memes</h3>
+              </div>
+              <div className="my-meme">
+                <img src="/temp-images/temp-meme-image.jpeg" alt="Meme 2" />
+                <h3>This is one of my memes</h3>
+              </div>
+              <div className="my-meme">
+                <img src="/temp-images/temp-meme-image.jpeg" alt="Meme 3" />
+                <h3>This is one of my memes</h3>
+              </div>
+            </div>  
+          </div>
         </div>
-      </div>
 
-      <div className="column">
-        <div className="card my-memes-card">
-          <div className="my-memes">
-            <h2>My Memes</h2>
-            <div className="my-meme">
-              <img src="/temp-images/temp-meme-image.jpeg" alt="Meme 1" />
-              <h3>This is one of my memes</h3>
-            </div>
-            <div className="my-meme">
-              <img src="/temp-images/temp-meme-image.jpeg" alt="Meme 2" />
-              <h3>This is one of my memes</h3>
-            </div>
-            <div className="my-meme">
-              <img src="/temp-images/temp-meme-image.jpeg" alt="Meme 3" />
-              <h3>This is one of my memes</h3>
-            </div>
-          </div>  
         </div>
-      </div>
 
       </div>
-
     </div>
+  </>
   );
 };
 
