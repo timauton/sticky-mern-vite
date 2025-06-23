d62fbe4e6a4d54070619c3f5829a78f1bedca410
Sarah Mathieson <sarahmathieson@nationwide.co.uk>
2025-06-23 16:23:44 +0100

    crossover

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,35 +1,57 @@
-import Button from "../../components/ButtonComponent";
-import { useState, useEffect, useRef } from "react";
-import { Login } from "../../components/Login";
-import { Signup } from "../../components/Signup";
-import MemeDisplay from "../../components/MemeDisplay";
-import { RatingBar } from "../../components/RatingBar";
-import MemeUploadButton from "../../components/MemeUploadButtonComponent";
-import { useNavigate } from "react-router-dom";
-import getMeme from "../../services/memeSelector";
+import Button from "../../components/ButtonComponent"
+import { useState, useEffect, useRef } from "react"
+import { Login } from "../../components/Login"
+import { Signup } from "../../components/Signup"
+import MemeDisplay from "../../components/MemeDisplay"
+import { RatingBar } from "../../components/RatingBar"
+import MemeUploadButton from "../../components/MemeUploadButtonComponent"
+import { useNavigate } from "react-router-dom"
+import getMeme from "../../services/memeSelector"
 
 import "../../index.css";
 
 // logged out render
 export function HomePage() {
+  // Meme Display
+  const [meme, setMeme] = useState([]);
+  let lastMeme = useRef(null);
+  useEffect(() => {
+    updateMeme("next");
+  }, [])
+
+  // Back and forth buttons
+  const updateMeme = (id) => {
+    const token = localStorage.getItem("token");
+    getMeme(token, id).then((data) => {
+      setMeme(data.meme)
+    })
+  }
+
+  const handleNextClick = () => {
+    lastMeme.current = meme;
+    console.log("LAST MEME", lastMeme)
+    updateMeme("next");
+  }
+
+  const handleBackClick = () => {
+    if (lastMeme.current) {
+      updateMeme(lastMeme.current._id);
+      lastMeme.current = null;
+    }
+  }
+
   // Login function
   const [showLogin, setShowLogin] = useState(false);
   const handleLoginClick = () => {
     setShowLogin((prev) => !prev);
-    setShowSignup(false);
+    setShowSignup(false)
   };
-
-  // Log out function
-  const handleLogOutClick = () => {
-    localStorage.removeItem("token");
-    setIsLoggedIn(false);
-  }
 
   // Signup function
   const [showSignup, setShowSignup] = useState(false);
   const handleSignupClick = () => {
     setShowSignup((prev) => !prev);
-    setShowLogin(false);
+    setShowLogin(false)
   };
 
   // This function will be passed to the Signup component
@@ -38,50 +60,12 @@ export function HomePage() {
     setShowLogin(true); // Show login form
   };
   
-  // For conditional rendering of page
   const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
+
   const handleLoginSuccess = () => {
-    setShowLogin(false);
-    setIsLoggedIn(true);
+    setShowLogin(false)
+    setIsLoggedIn(true)
   };
-
-  // Meme Display
-  const [meme, setMeme] = useState([]);
-  let lastMeme = useRef(null); // for back button
-  let nextMeme = useRef(null); // for forwards button after having pressed back
-  useEffect(() => {
-    if (isLoggedIn) {
-      updateMeme("next");
-    }
-  }, [isLoggedIn])
-
-  // Back and forth buttons
-  const updateMeme = (id) => {
-    if (!isLoggedIn) {
-      return;
-    }
-    const token = localStorage.getItem("token");
-    getMeme(token, id).then((data) => {
-      setMeme(data.meme);
-    })
-  }
-
-  const handleNextClick = () => {
-    lastMeme.current = meme;
-    if (nextMeme.current) {
-      updateMeme(nextMeme.current._id)
-      nextMeme.current = null;
-    }
-    updateMeme("next");
-  }
-
-  const handleBackClick = () => {
-    nextMeme.current = meme // sets 'next' button to be previously seen meme
-    if (lastMeme.current) {
-      updateMeme(lastMeme.current._id);
-      lastMeme.current = null;
-    }
-  }
 
   const navigate = useNavigate();
   
@@ -100,12 +84,12 @@ export function HomePage() {
           <Button className="login-button" buttonText={"Login"} onClick={handleLoginClick}/>
           </div>
           <div className="title">Sticky Memes</div>
-          <div className="image-container">TEXT
+          <div className="image-container">
             <img src="/The-archives.jpg" className="responsive-image" alt="the archives" />
           </div>
-          <div className="rating-bar-div">
+          {/* <div className="rating-bar-div">
             <RatingBar />
-          </div>
+          </div> */} {/* Moved to logged in render */}
         </div>
       </div>
   </>
@@ -124,37 +108,40 @@ export function HomePage() {
         </div>
         <Button
           className="stats-nav-button"
-          buttonText="My Stats" // other text is available
+          buttonText={"My\nStats"} // other text is available
           onClick={() => navigate("/stats")}
         />
         <Button
           className="logout-button"
           buttonText="Log Out"
-          onClick={handleLogOutClick}
+          onClick={() => {localStorage.removeItem("token"); setIsLoggedIn(false);}}
         />
       </div>
       <div className="title">Sticky Memes</div>
       <div className="meme-interface">
         {lastMeme !== null ? (<Button
           className="back-and-forth"
-          buttonText="<"
+          buttonImage="./left-arrow.png"
           onClick={handleBackClick}
         />) : (
           <Button
           className="back-and-forth"
-          buttonText="<"
-          disabled={true}/>
+          buttonImage="./left-arrow.png"
+          disabled={true}>
+            <span className="button-text-shadow">&lt;</span>
+          </Button>
         )}
         <MemeDisplay
           meme={meme}
         />
         <Button
           className="back-and-forth"
-          buttonText=">"
+          buttonImage="./right-arrow.png"
           onClick={handleNextClick}
         />
       </div>
     </>
   );
 }
+
 
