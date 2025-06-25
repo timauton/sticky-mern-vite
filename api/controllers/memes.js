e3ff6eb99f270b7753d1892dfeacad05afc08118
Tim Auton <tim@uton.org>
2025-06-25 12:26:45 +0100

    Algo working an tandem with search

--- api/controllers/memes.js
+++ api/controllers/memes.js
@@ -103,27 +103,20 @@ async function getNextMeme(req, res) {
     let token;
     try {
         token = generateToken(req.user_id);
+
         const tags = req.query.tags?.split(",").map(tag => tag.trim().toLowerCase());
 
         let memes;
+        console.log("---- getNextMeme ----");
         if (tags && tags.length > 0) {
-            memes = await Meme.aggregate([
-                { $match: { tags: { $all: tags } } },
-                { $sample: { size:1 } }
-            ]);
+            console.log("Getting memes with tags: " + tags);
+            memes = await getMemesWithTags(tags);
         } else {
-            memes = await Meme.aggregate([
-                { $sample: { size: 1} }
-            ])
+            console.log("Getting a random unrated meme");
+            memes = await getRandomUnratedMeme(req.user_id);
         }
 
         const meme = memes[0];
-        // get this user's ratings
-        const nextTag = await getNextRandomMemeTag(req.user_id);
-        console.log("Next tag is: " + nextTag);
-
-        // get any random meme
-        //const meme = await getRandomUnratedMeme(req.user_id);
 
         res.status(200).json({ meme: meme, token: token });
     }
@@ -217,8 +210,23 @@ async function getAllTags(req, res) {
     }
 }
 
-async function getRandomUnratedMeme(user_id) {
+async function getMemesWithTags(tags) {
     const memes = await Meme.aggregate([
+        { $match: { tags: { $all: tags } } },
+        { $sample: { size:1 } }
+    ]);
+    return memes;
+}
+
+async function getRandomUnratedMeme(user_id) {
+
+    const tag = await getNextRandomMemeTag(user_id);
+
+    console.log("Filtering to tag: " + tag);
+
+    // we need to construct different pipelines depending on what we're looking for
+    // we start with finding unrated ones
+    const unratedPartialPipeline = [
         // match with the Ratings schema
         { $lookup: {
                 from: 'ratings',
@@ -234,15 +242,83 @@ async function getRandomUnratedMeme(user_id) {
                 }
             }
         },
-        // pick one at random
-        { $sample: {
-                size:1
-            }
-        }
-    ]);
-    const meme = memes[0];
+    ];
 
-    return meme;
+    // construct the pipeline
+    // we may or may not need the tag filter,
+    // depending on what the algo suggested
+    let pipeline = [...unratedPartialPipeline]; //. this syntax is so we create a copy, not a reference
+    if (tag) {
+        pipeline.push({ $match: { tags: tag } });
+    }
+    pipeline.push({ $sample: { size: 1 } });
+
+    // run the query
+    let memes = await Meme.aggregate(pipeline);
+
+    // did we get a result?
+    if (memes.length == 0) {
+        console.log("There are no unrated memes with that tag, going for a completely random unrated meme");
+        // construct the pipeline without the tag filter
+        pipeline = [...unratedPartialPipeline];
+        pipeline.push({ $sample: { size: 1 } });
+        memes = await Meme.aggregate(pipeline);
+    }
+
+    // make sure again that we have a result
+    if (memes.length == 0) {
+        console.log("They've rated every single meme! Just pick one at random");
+        memes = await Meme.aggregate([
+            // pick one at random
+            { $sample: { size:1 } }
+        ]);
+    }
+
+    return memes;
+}
+
+async function getNextRandomMemeTag(user_id) {
+
+    // we add up all the ratings to get a total rating score
+    // then we add 20% to ensure a user doesn't always see the same memes
+    // then we pick a random number between 0 and the total score
+    // we work through the array starting at the highest rating and stop
+    // when we get to the random number
+
+    // Eg. a user has rated cats 5* and programming 1*
+    // Total number will be ( 5 + 1 ) * 1.2 = 7.2
+    // 3 would be cats
+    // 5.5 would be programming
+    // 6.4 would be random
+
+    // I have no idea how well this works in practice, but it does bias
+    // results towards the memes users like
+
+    // 1.25 here would give a 20% chance of totally random meme
+    const randomUnratedTagMultiplier = 1.25;
+
+    let totalRating = 0;
+    const ratedTags = await getRatedTags(user_id);
+
+    ratedTags.forEach((tag) => totalRating += tag.avg );
+
+    tagPickNumber = Math.random() * totalRating * randomUnratedTagMultiplier;
+
+    console.log("Pick number is: " + tagPickNumber);
+    console.log("Total rating is: " + totalRating);
+
+    let nextTag = "";
+
+    // step through and set the tag if we get a match
+    for (const tag of ratedTags) {
+        tagPickNumber -= tag.avg;
+        if (tagPickNumber < 0) {
+            nextTag = tag._id;
+            break;
+        }
+    }
+
+    return nextTag;
 }
 
 async function getRatedTags(user_id) {
@@ -280,51 +356,6 @@ async function getRatedTags(user_id) {
     console.log(tags);
 
     return tags;
-}
-
-async function getNextRandomMemeTag(user_id) {
-
-    // we add up all the ratings to get a total rating score
-    // then we add 20% to ensure a user doesn't always see the same memes
-    // then we pick a random number between 0 and the total score
-    // we work through the array starting at the highest rating and stop
-    // when we get to the random number
-
-    // Eg. a user has rated cats 5* and programming 1*
-    // Total number will be ( 5 + 1 ) * 1.2 = 7.2
-    // 3 would be cats
-    // 5.5 would be programming
-    // 6.4 would be random
-
-    // I have no idea how well this works in practice, but it does bias
-    // results towards the memes users like
-
-    // 1.25 here would give a 20% chance of totally random meme
-    const randomUnratedTagMultiplier = 1.25;
-
-    let totalRating = 0;
-    const ratedTags = await getRatedTags(user_id);
-
-    ratedTags.forEach((tag) => totalRating += tag.avg );
-
-    tagPickNumber = Math.random() * totalRating * randomUnratedTagMultiplier;
-
-    console.log("Pick number is: " + tagPickNumber);
-    console.log("Total rating is: " + totalRating);
-
-    nextTag = "";
-
-    for (const tag of ratedTags) {
-        tagPickNumber -= tag.avg;
-        if (tagPickNumber < 0) {
-            nextTag = tag._id;
-            break;
-        }
-    }
-
-    console.log(nextTag);
-
-    return nextTag;
 }
 
 const MemesController = {
